d639d7c170f77a2c749a643755ca58c3e8568b80
Haroon Ahmad <haroon334@gmail.com>
2021-11-29 15:14:54 +0100

    Router Added

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <!-- TODO Application entry point. Login view -->
-<?php require_once("./src/library/sessionHelper.php");
-//require_once("./lib/classes");
-$warning = checkSession();
+<?php
+
+require_once("./lib/database.php");
+require_once("./lib/classes/Controller.php");
+require_once("./lib/classes/Model.php");
+require_once("./lib/classes/Router.php");
+require_once("./lib/classes/Session.php");
+require_once("./controllers/SessionController.php");
 
 
-
+$router = new Router();
--- lib/classes/Session.php
+++ lib/classes/Session.php
@@ -2,4 +2,18 @@
 
 class Session
 {
+
+    function checkSession()
+    {
+        if (!isset($_SESSION["email"])) {
+            header("Location:  view/Login/index.php");
+        } else if (isset($_GET["logout"])) {
+            header("refresh:5,  view/Login/index.php");
+        } else if (isset($_SESSION["wrongLogin"])) {
+            $errorMsg =  $_SESSION["wrongLogin"];
+            unset($_SESSION);
+            session_destroy();
+            return ["type" => "danger", "text" => $errorMsg];
+        }
+    }
 }

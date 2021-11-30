46c7c82e3c3ebad4f29d9315da1b1f57b82d9512
Haroon Ahmad <haroon334@gmail.com>
2021-11-30 10:10:05 +0100

    Ok

--- controllers/sessionController.php
+++ controllers/sessionController.php
@@ -10,7 +10,7 @@ class SessionController extends Controller
     function checkSession()
     {
         if (!isset($_SESSION["email"])) {
-            header("Location: index.php");
+            header("Location:  view/Login/index.php");
         } else if (isset($_GET["logout"])) {
             header("refresh:5,  view/Login/index.php");
         } else if (isset($_SESSION["wrongLogin"])) {

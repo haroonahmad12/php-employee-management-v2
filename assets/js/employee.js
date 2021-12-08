1c24b20ec0fe95b42c0e21aee846b9d53d54cd7b
Haroon Ahmad <haroon334@gmail.com>
2021-12-08 13:17:50 +0100

    Form Details

--- assets/js/employee.js
+++ assets/js/employee.js
@@ -12,9 +12,58 @@ document.addEventListener("DOMContentLoaded", () => {
   )
     .then((res) => res.json())
     .then((res) => {
-      console.log(res);
+      createForm(res);
     })
     .catch(function () {
       alert("Can't connect to backend try latter");
     });
 });
+
+function createForm(data) {
+  let formTemplate = `<div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputName">Name</label>
+                    <input type="text" class="form-control" id="inputName" placeholder='${data.name}' name="name" value="${data.name}">
+                </div>
+                
+                <div class="form-group col-md-6">
+                    <label for="inputEmail">Email</label>
+                    <input type="email" class="form-control" id="inputEmail" placeholder='${data.email}' name="email" value='${data.email}'>
+                </div>
+                
+                <div class="form-group col-md-6">
+                    <label for="inputAge">Age</label>
+                    <input type="number" class="form-control" id="inputAge" name="age" value='${data.age}'>
+                </div>
+                <div class=" form-group col-md-6">
+                    <label for="inputPhone">Phone Number</label>
+                    <input type="text" class="form-control" id="inputPhone" name="phoneNumber" value='${data.phone_number}'>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputCity">City</label>
+                    <input type="text" class="form-control" id="inputCity" name="city" value='${data.city}'>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputAddress">Street No.</label>
+                    <input type="text" class="form-control" id="inputAddress" placeholder='${data.street_no}' name="streetAddress" value="${data.street_no}">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputState">State</label>
+                    <input type="text" class="form-control" id="inputState" name="state" value="${data.state}">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputZip">Zip Code</label>
+                    <input type="text" class="form-control" id="inputZip" name="postalCode" value="${data.postal_code}">
+                </div>
+            </div>
+
+
+
+            <input type="submit" class="btn btn-primary" value="Submit" id="submitButton"></input>
+            <input type="hidden" name="id" value=""></input>
+            <button type=" button" class="btn btn-secondary"><a href="../show" class="text-decoration-none btn-secondary">Return </a></button>`;
+
+  document
+    .getElementById("employeeDetailForm")
+    .insertAdjacentHTML("afterbegin", formTemplate);
+}
